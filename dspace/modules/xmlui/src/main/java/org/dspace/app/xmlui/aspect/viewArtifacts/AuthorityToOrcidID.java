e8c6fc72d18f0d70bfb06ec9b28473deb5425fc6
Jonas Van Goolen <jonas@atmire.com>
2015-09-25 13:32:35 +0200

    28298 Changed AuthorityToOrcidID class+ added spacing for authors (who have orcid links)

--- dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/viewArtifacts/AuthorityToOrcidID.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/app/xmlui/aspect/viewArtifacts/AuthorityToOrcidID.java
@@ -1,37 +1,50 @@
 package org.dspace.app.xmlui.aspect.viewArtifacts;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.dspace.authority.AuthoritySearchService;
+import org.dspace.authority.AuthorityValue;
+import org.dspace.authority.AuthorityValueFinder;
+import org.dspace.authority.orcid.OrcidAuthorityValue;
+import org.dspace.core.Context;
 import org.dspace.utils.DSpace;
+
+import java.sql.SQLException;
 
 /**
  * Created by jonas on 22/09/15.
  */
 public class AuthorityToOrcidID {
 
-    public String getOrcidID(String authorityValue) throws Exception {
+    private Logger log = Logger.getLogger(AuthorityToOrcidID.class);
 
-        SolrQuery queryArgs = new SolrQuery();
-        queryArgs.setQuery("*:*");
-        queryArgs.addFilterQuery("id:" + authorityValue);
-
-        if (getSearchService().search(queryArgs).getResults().size() > 0) {
-            try {
-                return (String) getSearchService().search(queryArgs).getResults().get(0).get("orcid_id");
-            } catch (Exception e) {
-                return null;
+    public String getOrcidID(String authorityValue) {
+        // Extra check for the authorityValue, should previously be checked in the xsl to see if authoriy even exist in the first place, <xsl:if test="@authority/>, only then call this method
+        if (StringUtils.isBlank(authorityValue)) {
+            return null;
+        }
+        AuthorityValueFinder authorityValueFinder = new AuthorityValueFinder();
+        Context context = null;
+        try {
+            context = new Context();
+            // If authorityValueFinder can't find an id, a null value will be returned
+            AuthorityValue authorityValueInstance = authorityValueFinder.findByUID(context, authorityValue);
+            if (authorityValueInstance instanceof OrcidAuthorityValue) {
+                if (authorityValueInstance != null) {
+                    return ((OrcidAuthorityValue) authorityValueInstance).getOrcid_id();
+                }
             }
 
+        } catch (SQLException e) {
+            log.error("An exception has occurred while searching for the orcid id", e);
+        } finally {
+            if (context != null) {
+                context.abort();
+            }
         }
         return null;
-    }
 
-    public static AuthoritySearchService getSearchService() {
-        DSpace dspace = new DSpace();
-
-        org.dspace.kernel.ServiceManager manager = dspace.getServiceManager();
-
-        return manager.getServiceByName(AuthoritySearchService.class.getName(), AuthoritySearchService.class);
     }
 
 }
