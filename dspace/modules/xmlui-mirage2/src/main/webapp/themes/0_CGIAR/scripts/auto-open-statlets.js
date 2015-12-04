d69bdf295ddbce37efb8dc51a566986e3df90c02
Antoine Snyers <antoine@atmire.com>
2015-12-04 11:41:12 +0100

    30052: Make auto show statlets configurable

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/scripts/auto-open-statlets.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/0_CGIAR/scripts/auto-open-statlets.js
@@ -1,14 +1,19 @@
-(function($) {
+(function ($) {
+    window.DSpace = window.DSpace || {};
+    window.DSpace.config = window.DSpace.config || {};
     window.atmire = window.atmire || {};
     window.atmire.CUA = window.atmire.CUA || {};
     window.atmire.CUA.statlet = window.atmire.CUA.statlet || {};
-    var afterInitCallbacks = window.atmire.CUA.statlet.afterInitCallbacks = window.atmire.CUA.statlet.afterInitCallbacks || [];
-    $(function() {
-        afterInitCallbacks.push(function() {
-            var statsWrapper = $('#aspect_statistics_StatletTransformer_div_statswrapper');
-            if (statsWrapper.length === 0 || statsWrapper.is(':hidden')) {
-                $('#aspect_statistics_StatletTransformer_div_showStats').find('.btn').click();
-            }
+
+    if (window.DSpace.config['atmire-cua.auto-open-statlets'] === 'true') {
+        var afterInitCallbacks = window.atmire.CUA.statlet.afterInitCallbacks = window.atmire.CUA.statlet.afterInitCallbacks || [];
+        $(function () {
+            afterInitCallbacks.push(function () {
+                var statsWrapper = $('#aspect_statistics_StatletTransformer_div_statswrapper');
+                if (statsWrapper.length === 0 || statsWrapper.is(':hidden')) {
+                    $('#aspect_statistics_StatletTransformer_div_showStats').find('.btn').click();
+                }
+            });
         });
-    });
+    }
 })(jQuery);
