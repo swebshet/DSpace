726a2deb28a716976889c99ce3092f4e5800e6be
Philip Vissenaekens <philipvissenaekens@atmire.com>
2017-01-10 13:21:06 +0100

    project setup - missing registries

--- dspace-api/src/main/java/org/dspace/storage/rdbms/DatabaseRegistryUpdater.java
+++ dspace-api/src/main/java/org/dspace/storage/rdbms/DatabaseRegistryUpdater.java
@@ -68,13 +68,8 @@ public class DatabaseRegistryUpdater implements FlywayCallback
             MetadataImporter.loadRegistry(base + "dcterms-types.xml", true);
             MetadataImporter.loadRegistry(base + "eperson-types.xml", true);
             MetadataImporter.loadRegistry(base + "sword-metadata.xml", true);
-
-            // Check if XML Workflow is enabled in workflow.cfg
-            if (ConfigurationManager.getProperty("workflow", "workflow.framework").equals("xmlworkflow"))
-            {
-                // If so, load in the workflow metadata types as well
-                MetadataImporter.loadRegistry(base + "workflow-types.xml", true);
-            }
+            MetadataImporter.loadRegistry(base + "workflow-types.xml", true);
+            MetadataImporter.loadRegistry(base + "cgiar-types.xml", true);
 
             context.restoreAuthSystemState();
             // Commit changes and close context
@@ -109,7 +104,9 @@ public class DatabaseRegistryUpdater implements FlywayCallback
     @Override
     public void afterInfo(Connection connection)
     {
-        // do nothing
+        if (!freshInstall) {
+            updateRegistries();
+        }
     }
 
     @Override
